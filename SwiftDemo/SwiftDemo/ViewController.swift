5f067179d8e27242ba6046fb29b9fb446f44f68e
amritpurandare <amrutpurandare213@gmail.com>
2019-08-22 11:36:30 +0530

    Initial project set up

---
+++ SwiftDemo/SwiftDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftDemo
+//
+//  Created by Devayani Purandare on 22/08/19.
+//  Copyright © 2019 Devayani Purandare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
